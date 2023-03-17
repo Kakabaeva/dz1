8617522f36d0f44f8125e2e97c21aea5fe4aa743
Эмина Какабаева <eminakakabaeva@MacBook-Air-Emina.local>
2023-03-17 21:29:10 +0300

    dz1-3

--- Program.cs
+++ Program.cs
@@ -26,16 +26,31 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-Console.WriteLine("Введите первое число");
+// Console.WriteLine("Введите первое число");
+// int number1=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int number3=Convert.ToInt32(Console.ReadLine());
+// int max=number1;
+// if (max <= number2)
+// max=number2;
+// if (max <= number3)
+// max=number3;
+// Console.Write("Максимальное число ");
+// Console.Write(max);
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите любое целое число");
 int number1=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int number2=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int number3=Convert.ToInt32(Console.ReadLine());
-int max=number1;
-if (max <= number2)
-max=number2;
-if (max <= number3)
-max=number3;
-Console.Write("Максимальное число ");
-Console.Write(max);
+if (number1%2==0)
+{
+    Console.Write("Введенное число "+number1+" четное");
+}
+else
+{
+    Console.Write("Введенное число "+number1+" нечетное");
+}
